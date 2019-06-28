b45576703cb3ab099f2f320570d9473c9d2882d6
Pwneill <phelan.oneill@gmail.com>
2019-06-27 20:36:17 -0500

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# clicky-game
-
-A React-based application based on the game "Memory". Users can click a potential (or announced) 2020 Presidential candidate to score points. However, if the User clicks the same candidate more than once, the game is over and their score is reset. 
-
-A live version of the game can be found at:  https://pwneill.github.io/clicky-game/. Users can also clone the repository, install the relevant node packages and then run "npm start" to launch the game on their local server.
-
-This project is powered using Express, Bootstrap and React.
-
--- precache-manifest.ee0afe20887c4148e837a19394b0c912.js
+++ precache-manifest.ee0afe20887c4148e837a19394b0c912.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
   },
   {
-    "revision": "a4ee4ad19ab1eaaffc72",
-    "url": "/clicky-game/static/js/main.a4ee4ad1.chunk.js"
+    "revision": "7a3ba209c0119c967163",
+    "url": "/clicky-game/static/js/main.7a3ba209.chunk.js"
   },
   {
     "revision": "ecddac34e8b105dff208",
     "url": "/clicky-game/static/js/1.ecddac34.chunk.js"
   },
   {
-    "revision": "a4ee4ad19ab1eaaffc72",
+    "revision": "7a3ba209c0119c967163",
     "url": "/clicky-game/static/css/main.30d4f84f.chunk.css"
   },
   {
-    "revision": "b60c6b77b36fcff64c279c860298cb1e",
+    "revision": "e816fc39f344ea3bc324778d6cc0b097",
     "url": "/clicky-game/index.html"
   }
 ];
